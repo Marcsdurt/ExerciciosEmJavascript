1de5d71d28144980dea8d76b0567941c97fb55b0
Marcos Duarte <106616718+Marcsdurt@users.noreply.github.com>
2024-01-30 16:24:12 -0300

    Create exercicio4.js
    o programa utiliza o "For" para varrer um array de números e retornar apenas o números pares

---
+++ exercicio4.js
@@ -0,0 +1,11 @@
+let lista = [1,2,3,4,5,6,7,8,9,10]
+
+
+for(let i = 0; i < lista.length; i++){
+    let resultado = lista[i]%2
+    
+    if(resultado === 0){
+        console.log(lista[i])
+    }
+   
+}
